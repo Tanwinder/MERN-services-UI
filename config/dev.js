ac175c50780cf522bb2c56761ebd0196f316f98b
tanwinder <tanwinder.ui@gmail.com>
2021-02-22 14:44:42 -0600

    update

--- config/dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    GOOGLE_CLIENT_ID: "191559063584-ffu0s5kpulqb5gel8todl0ehmr4ba8ig.apps.googleusercontent.com",
-    GOOGLE_CLIENT_SECRET: "7rfoHI-2Jrqn448iabr-pDPY",
-    MONGO_URI: "mongodb+srv://mern-service-prod:sXtmCJxFTROBKGPb@cluster1-prod.vscp2.mongodb.net/users?retryWrites=true&w=majority", // prod mongo
-    // MONGO_URI: "mongodb+srv://mern-service:5mIAqljCt0qy6fXX@cluster0.whqju.mongodb.net/test?retryWrites=true&w=majority",
-    COOKIE_KEY: "ddgwedgweiudgweiugdwjhwygwlfgwlifgwejgigfdscs",
-    PUBLISHABLE_KEY: "pk_test_51IKXY8EUXEaf7oYyPRzy9hXw0Igsjsj7AYIhuKU3FWIfVLVkCQmwRXNzs4FHOPXiwgFCBeFeEYm5gTcooZ3BXaJP00jimK4kPH",
-    SECRET_STRIPE_KEY: "sk_test_51IKXY8EUXEaf7oYyHHQHm1fx0PwnP1ZGPOJtOWmTHpq0z9KAC43qh3cI2eT9FwMoAMCCLlZDUSjysde3DGrop40500M4niC3Hg"
-}
